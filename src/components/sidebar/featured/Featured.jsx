6c7de07b5e3ba0a9db3606cab4555d62f5d45ad0
Shashikumarezhilarasu <shashikumarezhilarasu@gmail.com>
2024-04-29 16:21:04 +0530

    added

--- src/components/sidebar/featured/Featured.jsx
+++ src/components/sidebar/featured/Featured.jsx
@@ -2,6 +2,8 @@ import "/Users/shashikumarezhil/Documents/admindashboard/src/components/sidebar/
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 
 const Featured = () => {
@@ -19,6 +21,35 @@ const Featured = () => {
         <p className="title">Total sales made today</p>
         <p className="amount">$420</p>
         <p className="desc">previous transcation processing .last payments not included</p>
+
+        <div className="summary">
+            <div className="item">
+                <div className="itemTitle">Target</div>
+                <div className="itemResult negative">
+                    <ArrowDropDownIcon fontSize="small"/>
+                    <div className="resultAmount">$12.4k</div>
+                </div>
+
+            </div>
+
+            <div className="item">
+                <div className="itemTitle">Last Week</div>
+                <div className="itemResult positive">
+                    <KeyboardArrowUpIcon fontSize="small"/>
+                    <div className="resultAmount">$12.4k</div>
+                </div>
+
+            </div>
+
+            <div className="item">
+                <div className="itemTitle">Last Month</div>
+                <div className="itemResult positive">
+                    <KeyboardArrowUpIcon fontSize="small"/>
+                    <div className="resultAmount">$12.4k</div>
+                </div>
+
+            </div>
+        </div>
       </div>
     </div>
   )
