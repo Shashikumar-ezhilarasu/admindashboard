1aa162c24626a9823dd013a76c2eca3009e5dbb0
Shashikumarezhilarasu <shashikumarezhilarasu@gmail.com>
2024-04-29 14:39:56 +0530

    added

---
+++ src/components/widgets/Widget.jsx
@@ -0,0 +1,12 @@
+import '/Users/shashikumarezhil/Documents/admindashboard/src/components/widgets/widget.scss'
+
+const Widget = () => {
+  return (
+    <div className='widget'>
+      <div className="left">left</div>
+      <div className="right">right</div>
+    </div>
+  )
+}
+
+export default Widget
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@
 import Sidebar from "../../components/sidebar/Sidebar";
 import "/Users/shashikumarezhil/Documents/admindashboard/src/pages/home/home.scss";
 import Navbar from "../../components/navbar/Navbar"
+import { Widgets } from "@mui/icons-material";
+import Widget from "../../components/widgets/Widget";
 const Home = () => {
   return (
     <div className='home'>
@@ -10,7 +12,10 @@ const Home = () => {
       <div className="homeContainer">
       <Navbar />
       <div className="widgets">
-        
+        <Widget  />
+        <Widget  />
+        <Widget  />
+        <Widget  />
       </div>
       </div>
     </div>
